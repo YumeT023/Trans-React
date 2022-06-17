8a9e58d9c1105e50319fc0c2e2033f805e51092a
YumeT023 <hei.hajatiana@gmail.com>
2022-06-17 22:22:39 +0300

    project initialisation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello Toky_Transport</h1>
+    </div>
+  );
+}
+
+export default App;
